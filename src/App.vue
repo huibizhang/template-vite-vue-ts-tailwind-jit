87b94b1df82c98ca8c6f0f5e1541cd5f88a2ae4d
搋兔 <naruto6404@hotmail.com>
2021-07-30 20:26:15 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <img class="mx-auto mt-12 mb-8" alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite + Tailwind jit" />
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import HelloWorld from './components/HelloWorld.vue'
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+})
+</script>
+
+<style lang="postcss">
+  #app {
+    @apply text-center
+  }
+</style>
